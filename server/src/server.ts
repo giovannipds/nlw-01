acd207c2ff88b972760947040da85a19a10f713d
Giovanni Pires da Silva <giovanni@somosgad.com>
2020-06-05 18:23:21 -0300

    Explanation about RESTful routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,6 +5,15 @@ const app = express();
 // Rota: Endereço completo da requisição
 // Recurso: Qual entidade estamos acessando do sistema
 
+// GET: Buscar uma ou mais informações do back-end
+// POST: Criar uma nova informação no back-end
+// PUT: Atualizar uma informação existente no back-end
+// DELETE: Remover uma informação do back-end
+
+// POST http://localhost:3333/users - Criar um usuário
+// GET http://localhost:3333/users - Listar usuários
+// GET http://localhost:3333/users/5 - Buscar dados do usuário com ID 5
+
 app.get('/users', (request, response) => {
   console.log('Listagem de usuários');
 
