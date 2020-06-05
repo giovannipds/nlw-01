11662eb17347c3eeb541ebc0ddd4c008f10a4595
Giovanni Pires da Silva <giovanni@somosgad.com>
2020-06-05 18:20:32 -0300

    About routes and resources

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,9 @@ import express from 'express';
 
 const app = express();
 
+// Rota: Endereço completo da requisição
+// Recurso: Qual entidade estamos acessando do sistema
+
 app.get('/users', (request, response) => {
   console.log('Listagem de usuários');
 
