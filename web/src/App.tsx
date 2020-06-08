cc9881abd149dc7f135a88c8831ae116a5b57f06
Giovanni Pires da Silva <giovanni@somosgad.com>
2020-06-08 13:42:13 -0300

    Revert "Test it"
    This reverts commit f5d99176d922d17885d7ac004c27b02984c0ff11.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,11 +2,10 @@ import React from 'react';
 import './App.css';
 
 import Home from './pages/Home';
-import CreatePoint from './pages/CreatePoint';
 
 function App() {
   return (
-    <CreatePoint />
+    <Home />
   );
 }
 
