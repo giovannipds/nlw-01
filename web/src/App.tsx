88312673a43f98829fcc4c7b1778b2d68e84d476
Giovanni Pires da Silva <giovanni@somosgad.com>
2020-06-08 12:02:27 -0300

    Explanation of old react code

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <h1>Hello World</h1>
-  );
+  // return React.createElement('h1', {
+  //   children: 'Hello World'
+  // });
+
+  return <h1>Hello World</h1>
 }
 
 export default App;
