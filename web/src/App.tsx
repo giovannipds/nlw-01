f5d99176d922d17885d7ac004c27b02984c0ff11
Giovanni Pires da Silva <giovanni@somosgad.com>
2020-06-08 13:41:58 -0300

    Test it

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import './App.css';
 
 import Home from './pages/Home';
+import CreatePoint from './pages/CreatePoint';
 
 function App() {
   return (
-    <Home />
+    <CreatePoint />
   );
 }
 
