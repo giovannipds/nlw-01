aa26b058b734ba6dd0a95620ff43f7d29361071b
Giovanni Pires da Silva <giovanni@somosgad.com>
2020-06-08 12:59:41 -0300

    Write a counter that doesn't re-render the component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,11 +6,19 @@ import './App.css';
 import Header from './Header';
 
 function App() {
+  let counter = 1;
+
+  function handleButtonClick() {
+    counter++;
+    console.log(counter);
+  }
+
   return (
     <div>
       <Header title="Hello World" />
 
-      <h1>Conteúdo da aplicação</h1>
+      <h1>{counter}</h1>
+      <button type="button" onClick={handleButtonClick}>Aumentar</button>
     </div>
   );
 }
