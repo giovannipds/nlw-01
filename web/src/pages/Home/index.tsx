0e497588a9c1eab8f35c88bda32ed4f92e0d0d61
Giovanni Pires da Silva <giovanni@somosgad.com>
2020-06-08 13:17:35 -0300

    Insert home main texts

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -11,6 +11,11 @@ const Home = () => {
         <header>
           <img src={logo} alt="Ecoleta"/>
         </header>
+
+        <main>
+          <h1>Seu marketplace de coleta de resíduos.</h1>
+          <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</p>
+        </main>
       </div>
     </div>
   )
