70f3c5806f507b67c527b40cfcea84c8a5bf73be
Giovanni Pires da Silva <giovanni@somosgad.com>
2020-06-08 12:16:05 -0300

    Showing TypeScript prop error

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -8,8 +8,8 @@ import Header from './Header';
 function App() {
   return (
     <div>
-      <Header />
-      
+      <Header title="Hello World" />
+
       <h1>Conteúdo da aplicação</h1>
     </div>
   );
