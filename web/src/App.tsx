eae50601e46b63979bdd7ab7ba5fe1fb470c750b
Giovanni Pires da Silva <giovanni@somosgad.com>
2020-06-08 13:03:49 -0300

    Sample of state in a child component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -14,10 +14,9 @@ function App() {
 
   return (
     <div>
-      <Header title="Hello World" />
+      <Header title={`Contador: ${counter}`} />
 
       <h1>{counter}</h1>
-      <h1>{counter * 2}</h1>
       <button type="button" onClick={handleButtonClick}>Aumentar</button>
     </div>
   );
