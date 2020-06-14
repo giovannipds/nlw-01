c32990cc28c85e2f03cf55f7415b20ba483c2891
Giovanni Pires da Silva <giovannipds@gmail.com>
2020-06-14 14:24:36 -0300

    Create link back button

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
 
 import './styles.css';
 
@@ -9,6 +11,11 @@ const CreatePoint = () => {
     <div id="page-create-point">
       <header>
         <img src={logo} alt="Ecoleta"/>
+
+        <Link to="/">
+          <FiArrowLeft />
+          Voltar para home
+        </Link>
       </header>
     </div>
   );
