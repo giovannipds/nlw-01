6798a1e04d816a88d83d2ab0e246cda0d3e13725
Giovanni Pires da Silva <giovannipds@gmail.com>
2020-06-15 04:16:14 -0300

    Change View to Fragment and set barStyle

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -5,9 +5,9 @@ import Home from './src/pages/Home';
 
 export default function App() {
   return (
-    <View>
-      <StatusBar />
+    <>
+      <StatusBar barStyle="light-content" />
       <Home />
-    </View>
+    </>
   );
 }
