3d8e2f7e9c14ea4fc92817a314d86696f87013cd
Giovanni Pires da Silva <giovannipds@gmail.com>
2020-06-15 04:21:40 -0300

    Import logo using require

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { View } from 'react-native';
-
-import logo from '../../assets/logo.png';
+import { View, Image } from 'react-native';
 
 const Home = () => {
-  return <View />;
+  return (
+    <View>
+      <Image source={require('../../assets/logo.png')} />
+    </View>
+  );
 }
 
 export default Home;
