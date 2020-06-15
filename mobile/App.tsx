e429db6d0013b0ff2096255bda95e10cac98601c
Giovanni Pires da Silva <giovannipds@gmail.com>
2020-06-15 04:17:01 -0300

    Customize StatusBar

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -6,7 +6,7 @@ import Home from './src/pages/Home';
 export default function App() {
   return (
     <>
-      <StatusBar barStyle="light-content" />
+      <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent />
       <Home />
     </>
   );
