032499240c32ed608ce7512d4209ab5cd327fd62
Giovanni Pires da Silva <giovannipds@gmail.com>
2020-06-16 12:02:33 -0300

    Show that RN can apply an array of classes

--- mobile/src/pages/Points/index.tsx
+++ mobile/src/pages/Points/index.tsx
@@ -14,6 +14,8 @@ interface Item {
 }
 const Points = () => {
   const [items, setItems] = useState<Item[]>([]);
+  const [selectedItems, setSelectedItems] = useState<number[]>([]);
+
   const navigation = useNavigation();
 
   useEffect(() => {
@@ -28,6 +30,18 @@ const Points = () => {
 
   function handleNavigateToDetail() {
     navigation.navigate('Detail');
+  }
+
+  function handleSelectItem(id: number) {
+    const alreadySelected = selectedItems.findIndex(item => item === id);
+
+    if (alreadySelected >= 0) {
+      const filteredItems = selectedItems.filter(item => item !== id);
+
+      setSelectedItems(filteredItems);
+    } else {
+      setSelectedItems([ ...selectedItems, id ]);
+    }
   }
 
   return (
@@ -75,8 +89,11 @@ const Points = () => {
           {items.map(item => (
             <TouchableOpacity
               key={String(item.id)}
-              style={styles.item}
-              onPress={() => {}}
+              style={[
+                styles.item,
+                styles.selectedItem
+              ]}
+              onPress={() => handleSelectItem(item.id)}
               activeOpacity={0.6}
             >
               <SvgUri width={42} height={42} uri={item.image_url} />
