4c8c57f34d9fd20fa1097688412900be2db28f46
Giovanni Pires da Silva <giovannipds@gmail.com>
2020-06-16 17:58:59 -0300

    Validate with celebrate

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import cors from 'cors';
-import routes from './routes';
 import path from 'path';
+import routes from './routes';
+import { errors } from 'celebrate';
 
 const app = express();
 
@@ -10,6 +11,8 @@ app.use(express.json());
 app.use(routes);
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.use(errors());
 
 app.listen(3333);
  
