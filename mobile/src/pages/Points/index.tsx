22c80d95c3b69f6b91a81c96df43ef07cedb417c
Giovanni Pires da Silva <giovannipds@gmail.com>
2020-06-16 11:59:26 -0300

    Change opacity click from TouchableOpacity

--- mobile/src/pages/Points/index.tsx
+++ mobile/src/pages/Points/index.tsx
@@ -73,7 +73,12 @@ const Points = () => {
           contentContainerStyle={{ paddingHorizontal: 20 }}
         >
           {items.map(item => (
-            <TouchableOpacity key={String(item.id)} style={styles.item} onPress={() => {}}>
+            <TouchableOpacity
+              key={String(item.id)}
+              style={styles.item}
+              onPress={() => {}}
+              activeOpacity={0.6}
+            >
               <SvgUri width={42} height={42} uri={item.image_url} />
               <Text style={styles.itemTitle}>{item.title}</Text>
             </TouchableOpacity>
